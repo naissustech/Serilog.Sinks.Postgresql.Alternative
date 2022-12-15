c6d19b96eec3e6521c178a5f4c5009b915a4c688
Daniel Dimitrov <bojan.ristic@naissus.tech>
2022-12-15 09:26:36 +0100

    Error logs better handling

---
+++ src/Serilog.Sinks.Postgresql.Alternative/Sinks/PostgreSQL/LogPropertyExtension.cs
@@ -0,0 +1,30 @@
+﻿using Serilog.Events;
+
+namespace Serilog.Sinks.PostgreSQL;
+
+/// <summary>
+/// Extension methods for Log Properties
+/// </summary>
+public static class LogPropertyExtension
+{
+    /// <summary>
+    /// Extract string value of LogEventProperty
+    /// </summary>
+    /// <param name="logEventProperty">LogEventProperty to extract string value</param>
+    /// <returns>String vale of property</returns>
+    public static string PropertyToString(this LogEventProperty logEventProperty)
+    {
+        var stringValue = logEventProperty.Value?.ToString();
+        if (string.IsNullOrEmpty(stringValue))
+        {
+            return string.Empty;
+        }
+
+        if (stringValue.StartsWith("\"") && stringValue.EndsWith("\"") && stringValue.Length > 1)
+        {
+            return stringValue.Substring(1, stringValue.Length - 2);
+        }
+
+        return stringValue;
+    }
+}
